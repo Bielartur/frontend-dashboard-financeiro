61a3ba8f63788000a4ab98df984b50776a73911e
Gabriel Canto <gabrielcanto2016@gmail.com>
2026-01-08 15:15:32 -0300

    feat(models): atualiza definições de tipos para Categoria e Banco

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,4 +1,12 @@
 export interface Category {
   id: string;
   name: string;
+  colorHex: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface CategoryCreate {
+  name: string;
+  colorHex: string;
 }
